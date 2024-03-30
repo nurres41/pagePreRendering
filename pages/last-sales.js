5f6a429b1f4b6e9b776e73098dca6f4036ff4f6f
nurionurkurtulus <nurionurkurtulus@gmail.com>
2024-03-30 02:19:43 +0100

    Combining pre fetch with CSR fetching

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from "react";
-import useSWR from 'swr';
+import useSWR from "swr";
 
-const LastSalesPage = () => {
-  const [sales, setSales] = useState();
-  const [loading, setLoading] = useState(false);
-  
+const LastSalesPage = (props) => {
+  const [sales, setSales] = useState(props.sales);
+  // const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     setLoading(true);
     fetch("https://nextjs-course-4fbc3-default-rtdb.firebaseio.com/sales.json")
       .then((response) => response.json())
       .then((data) => {
-        
         const transformedSales = [];
 
         for (const key in data) {
@@ -19,7 +18,6 @@ const LastSalesPage = () => {
             username: data[key].username,
             volume: data[key].volume,
           });
-
         }
         setSales(transformedSales);
         setLoading(false);
@@ -27,24 +25,43 @@ const LastSalesPage = () => {
   }, []);
 
   if (loading) {
-    return <p>Loading...</p>
+    return <p>Loading...</p>;
   }
 
   if (!sales) {
-    return <p>No Data yet!</p>
+    return <p>No Data yet!</p>;
   }
 
   return (
-        <ul>
-            {
-                sales?.map(sale => (
-                    <li key={sale.id}>
-                        {sale.username} - ${sale.volume}
-                    </li>
-                ))
-            }
-        </ul>
-    );
+    <ul>
+      {sales?.map((sale) => (
+        <li key={sale.id}>
+          {sale.username} - ${sale.volume}
+        </li>
+      ))}
+    </ul>
+  );
 };
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://nextjs-course-4fbc3-default-rtdb.firebaseio.com/sales.json"
+  );
+  const data = response.json();
+
+  const transformedSales = [];
+
+  for (const key in data) {
+    transformedSales.push({
+      id: key,
+      username: data[key].username,
+      volume: data[key].volume,
+    });
+  }
+  return {
+    props: { sales: transformedSales },
+    revalidate: 10,
+  };
+}
 
 export default LastSalesPage;
